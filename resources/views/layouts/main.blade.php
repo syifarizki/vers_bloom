5ad9a1e7b98df22b7e9c684f9aa62efd53592c54
syifarizki <rizkisyifa379@gmail.com>
2023-12-01 16:41:17 +0700

    Menyelesaikan navbar

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+
+    <title>Vers Bloom | Home</title>
+</head>
+<body>
+    
+
+    @include('partials.navbar')
+    
+    <div class="container mt-60">
+        @yield('container')
+      </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
+</body>
+</html>
