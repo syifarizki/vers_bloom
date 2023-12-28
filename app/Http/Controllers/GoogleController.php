6234762c6a5abcacf6a9ec974a24c2318adf94da
audihezra21 <audiahezra21@gmail.com>
2023-12-28 14:04:14 +0700

    fix_login

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-
-
 class GoogleController extends Controller
 {
     public function redirectToGoogle()
@@ -18,35 +16,40 @@ class GoogleController extends Controller
     }
 
     public function handleGoogleCallback()
-    {
-        try {
-            $user = Socialite::driver('google')->user();
+{
+    try {
+        $user = Socialite::driver('google')->user();
 
-            // Cek apakah pengguna sudah terdaftar
-            $findUser = User::where('google_id', $user->getId())->first();
-            if ($findUser) {
-                Auth::login($findUser);
-                return redirect()->intended('dashboard');
-            } else {
-                // Jika belum terdaftar, buat pengguna baru
-                $newUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'google_id' => $user->id,
-                    'password' => bcrypt('123456')
-                    // tambahkan data lainnya sesuai kebutuhan
-                ]);
+        // Cek apakah pengguna sudah terdaftar
+        $findUser = User::where('google_id', $user->getId())->first();
+        if ($findUser) {
+            Auth::login($findUser);
+        } else {
+            // Jika belum terdaftar, buat pengguna baru
+            $newUser = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'google_id' => $user->id,
+                'password' => bcrypt('123456')
+                // tambahkan data lainnya sesuai kebutuhan
+            ]);
 
-                Auth::login($newUser);
-                return redirect()->intended('dashboard');
-                
-            }
-
-
-
-        } catch (\Exception $e) {
-            return redirect()->route('login')->with('error', 'Terjadi kesalahan saat menghandle callback: ' . $e->getMessage());
+            Auth::login($newUser);
         }
-        
+
+        // Periksa nilai is_admin
+        $isAdmin = Auth::user()->is_admin;
+
+        // Periksa apakah pengguna admin dan arahkan ke dashboard jika iya
+        if ($isAdmin == 1) {
+            return redirect()->route('dashboard'); // Ganti 'dashboard' dengan nama rute yang sesuai
+        } else {
+            return redirect()->intended('/');
+        }
+
+    } catch (\Exception $e) {
+        return redirect()->route('login')->with('error', 'Terjadi kesalahan saat menghandle callback: ' . $e->getMessage());
     }
+}
+
 }
