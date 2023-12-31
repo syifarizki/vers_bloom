70d4ba3f79e79cbdf918bb352f6894d9110eead2
rismrahmana <rismarahmanaff@gmail.com>
2023-12-31 18:59:32 +0700

    menyelesaikan main

--- resources/views/dashboard/content/home.blade.php
+++ resources/views/dashboard/content/home.blade.php
@@ -1,45 +1,32 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-    <div class="mgrid gap-5 p-6 sm:grid-cols-2 xl:grid-cols-3">
-        <!-- card2 -->
-        <div class="mt-10 ml-5">
-            <div class="shadow-soft-xl relative flex min-w-0 flex-col break-words rounded-2xl bg-blue-100 bg-clip-border">
-                <div class="flex-auto p-4">
-                    <div class="-mx-3 flex flex-row">
-                        <div class="w-2/3 max-w-full flex-none px-3">
-                            <div>
-                                <p class="mb-0 font-sans text-sm font-semibold leading-normal">Category</p>
-                                <h5 class="mb-0 font-bold">
-                                    2,300
-                                    <span class="font-weight-bolder text-sm leading-normal text-lime-500">+3%</span>
-                                </h5>
-                            </div>
+    <div class="bg-primary bg-opacity-20 mx-24 mt-14 rounded-xl">
+        <div class="flex items-center justify-between py-10 mt-5 px-10">
+        </div>
+    </div>
+
+    <div class="bg-primary bg-opacity-20 mx-24 mt-5 rounded-xl">
+        <div class="flex flex-wrap items-center justify-center pt-2 ">
+            <div class="grid grid-cols-3 gap-5 px-10">
+                <a href="/dashboard/posts">
+                    <div class="rounded-xl justify-center border-primary border-opacity-50 shadow-xl">
+                        <span class="text-xl text-center font-semibold pl-10 "> My Product</span>
+                        <div class="flex justify-center">
+                            <p class="pt-16 pl-20 font-bold text-xl">30</p>
                         </div>
                     </div>
-                </div>
+                </a>
+
+                <a href="/dashboard/categories">
+                    <div class="rounded-xl border-primary border-opacity-50 shadow-xl">
+                        <span class="text-xl text-center font-semibold pl-10 "> Categories</span>
+                        <div class="flex justify-center">
+                            <p class="pt-16 pl-20  font-bold text-xl">30</p>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
-        <!-- card2
-        <div class="mb-6">
-            <div class="shadow-soft-xl relative flex min-w-0 flex-col break-words rounded-2xl bg-blue-100 bg-clip-border">
-                <div class="flex-auto p-4">
-                    <div class="-mx-3 flex flex-row">
-                        <div class="w-2/3 max-w-full flex-none px-3">
-                            <div>
-                                <p class="mb-0 font-sans text-sm font-semibold leading-normal">Category</p>
-                                <h5 class="mb-0 font-bold">
-                                    2,300
-                                    <span class="font-weight-bolder text-sm leading-normal text-lime-500">+3%</span>
-                                </h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
     </div>
 @endsection
-
-
-
